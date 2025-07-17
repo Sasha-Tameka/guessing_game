82bc637753ea37fdbf1947e4bbad46b28038fb9d
Sasha-Tameka <sashh92@hotmail.com>
2025-07-17 16:59:48 -0400

    added variables to create tries limitations

--- guessing_game.py
+++ guessing_game.py
@@ -2,7 +2,15 @@
 
 secret_word = "Giraffe"
 guess = ""
+tries = 0
+tries_limit = 3
+out_of_guesses = False
 
 #prompt user
 
-wh
+while guess != secret_word:
+    guess = input("Enter first guess: ")
+    tries +=1
+print("You win!")
+
+
