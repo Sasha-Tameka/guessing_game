890ca03a81f1a445accb5f67f60b9d2dd08a5541
Sasha-Tameka <sashh92@hotmail.com>
2025-07-31 11:51:48 -0400

    added if statment if the first guess is correct

--- guessing_game.py
+++ guessing_game.py
@@ -1,22 +1,30 @@
+import random
 #Variables
 
-secret_word = "Giraffe"
-guess = ""
+secret_word = "giraffe"
 tries = 0
 tries_limit = 3
 out_of_guesses = False
+guess_correctly = False
 
 #prompt user
 
-while guess != secret_word and not out_of_guesses:
+while not guess_correctly and not out_of_guesses:
     if tries < tries_limit:
-        guess = input("Enter first guess: ")
+        guess = input("Enter first guess: ").lower()
+        if guess == secret_word:
+            guess_correctly = True
+            break
+        guess_2 = input("Enter second guess: ").lower()
+        guess_3 = input("Enter last guess: ").lower()
         tries +=1
+        if secret_word in [guess, guess_2,guess_3]:
+            guess_correctly= True
     else:
         out_of_guesses = True
-if out_of_guesses:
-    print("Out of Guesses, YOU LOSE!")
-else:
+if guess_correctly:
     print("You win!")
+else:
+    print("Out of Guesses, YOU LOSE!")
 
 
